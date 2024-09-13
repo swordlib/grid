81fbcabe12667ead407e740c6a293f86c34b705c
fallenleavesguy <9261562+fallenleavesguy@users.noreply.github.com>
2024-09-13 17:25:17 +0800

    fix: remove React does not recognize the  prop on a DOM element

--- packages/grid/src/Selection.tsx
+++ packages/grid/src/Selection.tsx
@@ -3,7 +3,14 @@ import { SelectionProps } from "./Grid";
 import { createHTMLBox } from "./utils";
 
 const Selection: React.FC<SelectionProps> = memo((props) => {
-  return createHTMLBox({ strokeWidth: 1, ...props });
+  const {   // ignore, remove react warning
+    inProgress,
+    selection,
+    borderStyle,
+  ...rest
+  } = props;
+
+  return createHTMLBox({ strokeWidth: 1, ...rest });
 });
 
 export default Selection;
